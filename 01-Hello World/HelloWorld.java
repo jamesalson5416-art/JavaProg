bc0d8724b2b175e36a56cf8fcb5def6eb527dcbb
Myo Khant Kyaw <jamesalson5416@gmail.com>
2025-08-21 13:49:13 +0800

    Update HelloWorld.java

--- 01-Hello World/HelloWorld.java
+++ 01-Hello World/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
     // Prints "Hello, World" to the terminal window.
     public static void main(String[] args)
     {
+		System.out.println("Hello, world");
         System.out.println("My name is James");
 		System.out.println("Course Name: OBJECT ORIENTED PROGRAMMING IN JAVA I5");
 		System.out.println("Course code: CSC200");
